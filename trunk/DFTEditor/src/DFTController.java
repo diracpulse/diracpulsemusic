15fdff4829dedce3d28fc18bf44869d9aba2b232
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2009-11-18 07:43:32 +0000

    DFTEditor factored into Model-View-controller

---
+++ trunk/DFTEditor/src/DFTController.java
@@ -0,0 +1,99 @@
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class DFTController implements MouseListener, ActionListener {
+	
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
+	public void mousePressed(MouseEvent e){}
+	
+	public void mouseClicked(MouseEvent e){
+		/*
+	    int xPos = e.getX();
+	    int yPos = e.getY();
+	    int freqIndex;
+	    int timeIndex;
+	    if ((xPos > leftOffset) && (yPos > upperOffset)) { 
+	    	timeIndex = (xPos - leftOffset) / xStep + leftX;
+	    	freqIndex = (yPos - upperOffset) / yStep + upperY;
+	    	if((freqIndex >= dataYDim) || (timeIndex >= dataXDim)) {
+		    	// OUT OF BOUNDS
+		    	System.out.println("OUT OF BOUNDS");
+		    	return;
+	    	}
+	    	// DATA CELL SELECTED
+	    	System.out.println("DATA: Freq = " + ((Float) freqs.get(freqIndex)) 
+	    						+ " | Time: " + (timeIndex * timeStep) 
+	    						+ " | Amplitude: " + data[timeIndex][freqIndex]);
+	    	return;
+    	}
+    	if (xPos <= leftOffset) {
+	    	if (yPos <= upperOffset) {
+		    	// UPPER LEFT CORNER SELECTED
+	    		System.out.println("UPPER LEFT CORNER");
+	    		return;
+    		}
+    		// LEFT FREQUENCY SELECTED
+    		freqIndex = (yPos - upperOffset) / yStep + upperY;
+    		System.out.println("FREQUENCY: " + ((Float) freqs.get(freqIndex)));
+    		return;
+    	}
+    	// UPPER TIME SELECTED
+    	timeIndex = (xPos - leftOffset) / xStep + leftX;
+	    System.out.println("TIME: " + (timeIndex * timeStep));
+	    return;
+	    */
+	}
+	
+    public void actionPerformed(ActionEvent e) {
+        int apOldUpperY = DFTEditor.upperY;
+        int apOldLeftX = DFTEditor.leftX;
+    if ("F+1".equals(e.getActionCommand())) {
+                    if((DFTEditor.upperY + 1) < DFTEditor.dataYDim) DFTEditor.upperY += 1; 
+    }
+    if ("F+6".equals(e.getActionCommand())) {
+                    if((DFTEditor.upperY + 6) < DFTEditor.dataYDim) DFTEditor.upperY += 6; 
+    }
+    if ("F+31".equals(e.getActionCommand())) {
+                    if((DFTEditor.upperY + 31) < DFTEditor.dataYDim) DFTEditor.upperY += 31; 
+    }         
+    if ("T+1".equals(e.getActionCommand())) {
+                    if((DFTEditor.leftX + 1) < DFTEditor.dataXDim) DFTEditor.leftX += 1; 
+    }
+    if ("T+10".equals(e.getActionCommand())) {
+                    if((DFTEditor.leftX + 10) < DFTEditor.dataXDim) DFTEditor.leftX += 10; 
+    }
+    if ("T+100".equals(e.getActionCommand())) {
+                    if((DFTEditor.leftX + 100) < DFTEditor.dataXDim) DFTEditor.leftX += 100; 
+    }        
+    if ("F-1".equals(e.getActionCommand())) {
+                    if((DFTEditor.upperY - 1) >= 0) DFTEditor.upperY -= 1; 
+    }
+    if ("F-6".equals(e.getActionCommand())) {
+                    if((DFTEditor.upperY - 6) >= 0) DFTEditor.upperY -= 6; 
+    }
+    if ("F-31".equals(e.getActionCommand())) {
+                    if((DFTEditor.upperY - 31) >= 0) DFTEditor.upperY -= 31; 
+    }  
+    if ("T-1".equals(e.getActionCommand())) {
+                    if((DFTEditor.leftX - 1) >= 0) DFTEditor.leftX -= 1; 
+    }
+    if ("T-10".equals(e.getActionCommand())) {
+                    if((DFTEditor.leftX - 10) >= 0) DFTEditor.leftX -= 10; 
+    }
+    if ("T-100".equals(e.getActionCommand())) {
+                    if((DFTEditor.leftX - 100) >= 0) DFTEditor.leftX -= 100; 
+    }        
+    if((apOldUpperY != DFTEditor.upperY) || (apOldLeftX != DFTEditor.leftX)) {
+           DFTEditor.view.DrawFileData(true);
+    }
+}
+
+    	
+}
