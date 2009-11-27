f08433b9644d6e4aaba6e79e8183efc99cb1b1e5
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2009-11-27 23:24:53 +0000

    Added amplitude sums to the top.

--- trunk/DFTEditor/src/DFTView.java
+++ trunk/DFTEditor/src/DFTView.java
@@ -7,6 +7,22 @@ import java.lang.*;
 import java.io.*;
 
 public class DFTView extends JComponent {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2964004162144513754L;
+
+	public void DrawAmpSums(Graphics g) {
+		int startX = DFTEditor.leftX;
+		int endX = DFTEditor.leftX + (getWidth() - DFTEditor.leftOffset) / DFTEditor.xStep;
+		for(int time = startX; time < endX; time++) {
+			int screenX = DFTEditor.leftOffset + ((time - startX) * DFTEditor.xStep);
+			if(DFTEditor.timeToAmpSum.get(new Integer(time)) == null) continue;
+			float ampSumVal = DFTEditor.timeToAmpSum.get(new Integer(time)).floatValue();
+			drawAmplitude(g, screenX, 0, ampSumVal, 0.0f, DFTEditor.maxAmplitudeSum);
+		}
+	}
 
 	public void DrawUpperTimes(Graphics g) {
 		int timeIndex;
@@ -24,7 +40,7 @@ public class DFTView extends JComponent {
 		for(timeIndex = startX; timeIndex < endX; timeIndex++) {
 			iTime = timeIndex;
 			leading0 = true;
-			int yOffset = 0;
+			int yOffset = 1;
 			for(digitPlace = 1000; digitPlace >= 1; digitPlace /= 10) {
 				digitVal = iTime / digitPlace;
 				if((digitVal == 0) && leading0 && (digitPlace != 1)) {
@@ -58,8 +74,6 @@ public class DFTView extends JComponent {
 
 
 	public void DrawLeftFreqs(Graphics g) {
-		int x;
-		int y;
 		int freqIndex;
 		int iFreq;
 		int digitPlace;
@@ -67,7 +81,6 @@ public class DFTView extends JComponent {
 		Color f;
 		Color b;
 		Color blank = new Color(0.0f, 0.0f, 0.0f);
-		int startX = 0;
 		int startY = DFTEditor.upperY;
 		int endY = startY + getHeight() - DFTEditor.upperOffset / DFTEditor.yStep;
 		int screenX;
@@ -107,65 +120,57 @@ public class DFTView extends JComponent {
 	public void DrawFileData(Graphics g, boolean scaleLines) {
 		DrawLeftFreqs(g);
 		DrawUpperTimes(g);
+		DrawAmpSums(g);
 		// clear old data
 		g.setColor(new Color(0.0f, 0.0f, 0.0f));
 		g.fillRect(DFTEditor.leftOffset, DFTEditor.upperOffset, getWidth(), getHeight());
+		TreeMap<Integer, Float> currentMap;
 		int startX = DFTEditor.leftX;
 		int endX = startX + ((getWidth() - DFTEditor.leftOffset) / DFTEditor.xStep);
 		int startY = DFTEditor.upperY;
 		int endY = startY + ((getHeight() - DFTEditor.upperOffset) / DFTEditor.yStep);
+		for(int x = startX; x < endX; x++) {
+            if(x >= DFTEditor.dataXDim) continue;
+            if(!DFTEditor.timeToFreqToAmp.containsKey(new Integer(x))) continue;
+            currentMap = DFTEditor.timeToFreqToAmp.get(new Integer(x));
+            for(int y = startY; y < endY; y++) {
+                if(y >= DFTEditor.dataYDim) continue;
+                if(!currentMap.containsKey(new Integer(DFTEditor.maxFreq - y))) continue;
+        		int screenX = DFTEditor.leftOffset + ((x - DFTEditor.leftX) * DFTEditor.xStep);
+        		int screenY = DFTEditor.upperOffset + ((y - DFTEditor.upperY) * DFTEditor.yStep);
+                float currentVal = currentMap.get(new Integer(DFTEditor.maxFreq - y)).floatValue();
+				drawAmplitude(g, screenX, screenY, currentVal, DFTEditor.minAmplitude, DFTEditor.maxAmplitude);
+			}
+		}
+	}
+	
+	public void drawAmplitude(Graphics g, int screenX, int screenY, float currentVal, float minVal, float maxVal) {
+		int digitVal;
+		int fractionVal;
 		float red;
 		float green;
 		float blue;
-		TreeMap<Integer, Float> currentMap;
-		float currentVal;
-		Float FVal;
-		float ampRange = DFTEditor.maxAmplitude - DFTEditor.minAmplitude;
-		Color b; // background
-		Color f = new Color(0.0f, 0.0f, 0.0f); // forground
-		Color black = new Color(0.0f, 0.0f, 0.0f);
-		Color white = new Color(1.0f, 1.0f, 1.0f);
-		Color gray = new Color(0.5f, 0.5f, 0.5f);
-		int digitVal;
-		float fDigitVal;
-		int fractionVal;
-		int screenX; // for whole part
-		int screenY;
-		int lowerScreenY; // for decimal part
-		int bottomScreenY;
-		// for(int x = 0; x < dataXDim; x++) {
-		//	for(int y = 0; y < dataYDim; y++) {
-		for(int x = startX; x < endX; x++) {
-			if(x >= DFTEditor.dataXDim) continue;
-			if(!DFTEditor.timeToFreqToAmp.containsKey(new Integer(x))) continue;
-			currentMap = DFTEditor.timeToFreqToAmp.get(new Integer(x));
-			for(int y = startY; y < endY; y++) {
-				if(y >= DFTEditor.dataYDim) continue;
-				if(!currentMap.containsKey(new Integer(DFTEditor.maxFreq - y))) continue;
-				currentVal = currentMap.get(new Integer(DFTEditor.maxFreq - y)).floatValue();
-				if(currentVal > 10.0f) {
-					digitVal = (int) Math.floor(currentVal);
-					digitVal -= 10;					
-				} else {
-					digitVal = (int) Math.floor(currentVal);
-				}
-				fractionVal = (int) Math.floor((currentVal - Math.floor(currentVal)) * 10.0f);
-				currentVal -= DFTEditor.minAmplitude;
-				blue = 1.0f - currentVal / ampRange;	
-				red = currentVal / ampRange;
-				if(red >= 0.5f) {
-					green = (1.0f - red) * 2.0f;
-				} else {
-					green = red * 2.0f;
-				}
-				// g.setColor(new Color(red, green, blue));
-				// g.fillRect(x * xStep, y * yStep, xStep, yStep);
-				b = new Color(red, green, blue);
-				screenX = DFTEditor.leftOffset + ((x - DFTEditor.leftX) * DFTEditor.xStep);
-				screenY = DFTEditor.upperOffset + ((y - DFTEditor.upperY) * DFTEditor.yStep);
-				DFTUtils.DrawSegmentData(g, b, screenX, screenY, digitVal, fractionVal); 
-			}
+		float ampRange = maxVal - minVal;
+		if(currentVal > 10.0f) {
+			digitVal = (int) Math.floor(currentVal);
+			digitVal -= 10;					
+		} else {
+			digitVal = (int) Math.floor(currentVal);
 		}
+		fractionVal = (int) Math.floor((currentVal - Math.floor(currentVal)) * 10.0f);
+		currentVal -= minVal;
+		currentVal /= ampRange;
+		blue = 1.0f - currentVal;
+		red = currentVal;
+		if(red >= 0.5f) {
+			green = (1.0f - red) * 2.0f;
+		} else {
+			green = red * 2.0f;
+		}
+		// g.setColor(new Color(red, green, blue));
+		// g.fillRect(x * xStep, y * yStep, xStep, yStep);
+		Color b = new Color(red, green, blue);
+		DFTUtils.DrawSegmentData(g, b, screenX, screenY, digitVal, fractionVal); 
 	}
 	
     protected void paintComponent(Graphics g) {
