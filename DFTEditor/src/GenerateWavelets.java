da7a76ebb6db38f1b96dc3d576667ab6d46d9046
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2010-01-27 01:59:00 +0000

    Made wavelet parameters independent of sampling rate.

--- DFTEditor/src/GenerateWavelets.java
+++ DFTEditor/src/GenerateWavelets.java
@@ -5,25 +5,24 @@ public class GenerateWavelets {
 
 	public static class WaveletParam {
 		
-		private int lengthInSamples;
-		private double radianFreq;
+		private double cyclesPerWindow;
+		private double freqInHz;
 		
-		WaveletParam(double radianFreq, int lengthInSamples) {
-			this.radianFreq = radianFreq;
-			this.lengthInSamples = lengthInSamples;
+		WaveletParam(double freqInHz, double cyclesPerWindow) {
+			this.freqInHz = freqInHz;
+			this.cyclesPerWindow = cyclesPerWindow;
 		}
 		
-		public int getLength() {
-			return lengthInSamples;
+		public double cyclesPerWindow() {
+			return cyclesPerWindow;
 		}
 		
-		public double getFreq() {
-			return radianFreq;
+		public double getFreqInHz() {
+			return freqInHz;
 		}
 		
 		public String toString() {
-			double freqInHz = (radianFreq / (Math.PI * 2.0)) * 44100.0;
-			return new String(freqInHz + " " + radianFreq + " " + lengthInSamples);
+			return new String(freqInHz + " " + cyclesPerWindow);
 		}
 		
 	}
@@ -34,7 +33,7 @@ public class GenerateWavelets {
 	private static final int MAXFREQUENCIES = 31 * 16;
 
 	static double samplingRate = 44100.0;
-	static double cyclesPerWindow = 45.22540955090449;
+	static double maxCyclesPerWindow = 45.22540955090449;
 	static double taperPerOctave = Math.sqrt(2.0);
 	double samplesPerStep = 220.5; // 5ms
 	double notesPerOctave = 31.0;
@@ -54,37 +53,25 @@ public class GenerateWavelets {
     
     static ArrayList<WaveletParam> InitFrequencies(double upperFreq, double centerFreq, double lowerFreq) {
     	ArrayList<WaveletParam> returnVal = new ArrayList<WaveletParam>();
-    	int freqIndex = 0;
-    	double freq;
-    	double radianFreq;
-    	double samplesPerCycle;
-    	double windowLength;
+    	double freqInHz;
+    	double cyclesPerWindow;
     	double taperValue;
-    	double ratio = (cyclesPerWindow - 1.0) / cyclesPerWindow;
+    	double ratio = (maxCyclesPerWindow - 1.0) / maxCyclesPerWindow;
     	if((ratio >= 1.0) || (ratio <= 0.0)) {
     		System.out.println("Error: InitFrequencies: invalid ratio: " + "ratio");
     		return null;
     	}
-    	for(freq = upperFreq; freq > centerFreq; freq *= ratio) {
-    		samplesPerCycle = samplingRate / freq;
-    		windowLength = samplesPerCycle * cyclesPerWindow;
-    		radianFreq = twoPI / samplesPerCycle;
-    		returnVal.add(new WaveletParam(radianFreq, (int) Math.round(windowLength)));
+    	for(freqInHz = upperFreq; freqInHz > centerFreq; freqInHz *= ratio) {
+    		returnVal.add(new WaveletParam(freqInHz, maxCyclesPerWindow));
     	}
     	// start tapering window length
-    	double startLogFreq = Math.log(freq) / Math.log(2.0);
+    	double startLogFreq = Math.log(freqInHz) / Math.log(2.0);
     	double currentLogFreq;
-    	for(; freq >= lowerFreq; freq *= ratio) {
-    		samplesPerCycle = samplingRate / freq;
-    		currentLogFreq =  Math.log(freq) / Math.log(2.0);
+    	for(; freqInHz >= lowerFreq; freqInHz *= ratio) {
+    		currentLogFreq =  Math.log(freqInHz) / Math.log(2.0);
     		taperValue = Math.pow(taperPerOctave, startLogFreq - currentLogFreq);
-    		windowLength = samplesPerCycle * (cyclesPerWindow / taperValue);
-    		radianFreq = twoPI / samplesPerCycle;
-    		returnVal.add(new WaveletParam(radianFreq, (int) Math.round(windowLength)));
-    	}
-    	if(freqIndex >= MAXFREQUENCIES) {
-    		System.out.println("Error: InitFrequencies: MAXFREQUENCIES exceeded, freq: " + freq + " index: " + freqIndex);
-    		System.exit(0);
+    		cyclesPerWindow = maxCyclesPerWindow / taperValue;
+    		returnVal.add(new WaveletParam(freqInHz, cyclesPerWindow));
     	}
     	return returnVal;
     }
