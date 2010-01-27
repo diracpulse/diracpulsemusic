71feccbfc471bf1da957c659d95957bfc256b6c1
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2010-01-27 06:19:36 +0000

    Working on endian stuff in Generate Wavelets.  Useful older code for synthesis and File Choosers.

--- trunk/DFTEditor/src/GenerateWavelets.java
+++ trunk/DFTEditor/src/GenerateWavelets.java
@@ -1,3 +1,6 @@
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 
 
@@ -27,51 +30,70 @@ public class GenerateWavelets {
 		
 	}
 	
-	final double gaussianConstant = 2.0;
-	final double onePI = Math.PI;
-	final static double twoPI = Math.PI * 2.0;
-	private static final int MAXFREQUENCIES = 31 * 16;
-
-	static double samplingRate = 44100.0;
 	static double maxCyclesPerWindow = 45.22540955090449;
 	static double taperPerOctave = Math.sqrt(2.0);
-	double samplesPerStep = 220.5; // 5ms
-	double notesPerOctave = 31.0;
-	int maxDFTLength = 0;
-	int inputFileLength = 0; 
-	int maxCenterIndex = 0;
-	int calcDFT = 0;
-	int stepIndex = 0;
+	static double noteBase = 31.0;
+	
+	public static void writeParamsToFile(String filename, ArrayList<WaveletParam> params) {
+		try {
+			FileOutputStream fos = new FileOutputStream(filename);
+	    	FileChannel fco = fos.getChannel();
+	    	ByteBuffer buffer = ByteBuffer.allocate(8);
+		} catch (Exception ex) {
+			// do nothing
+		}
+	    /*for (WaveletParam wp: params) {
+	      float freqInHz
+	      buffer.order(ByteOrder.BIG_ENDIAN);
+	      buffer.asFloatBuffer().put()
+	      int read = fci.read(buffer);
+
+	      if (read == -1)
+	        break;
+	      buffer.flip();
+	      fco.write(buffer);
+	      buffer.clear();
+	    }*/
+	}
 	
     public static void printParams()
     {
+    	double upperFreq = 20000.0;
+    	double centerFreq = 1000.0;
+    	double lowerFreq = 20.0;
+    	int upperNote = (int) Math.round(Math.log(upperFreq) / Math.log(2.0) * noteBase);
+    	int centerNote = (int) Math.round(Math.log(centerFreq) / Math.log(2.0) * noteBase);   	
+    	int lowerNote = (int) Math.round(Math.log(lowerFreq) / Math.log(2.0) * noteBase);   	
     	System.out.println("PARAMS");
-    	for(WaveletParam wp: InitFrequencies(18322.012048779428, 1000.0, 20.0)) {
+    	for(WaveletParam wp: InitFrequencies(upperNote, centerNote, lowerNote)) {
     		System.out.println(wp);
     	}
     }
     
-    static ArrayList<WaveletParam> InitFrequencies(double upperFreq, double centerFreq, double lowerFreq) {
+    static ArrayList<WaveletParam> InitFrequencies(int maxNote, int centerNote, int minNote) {
     	ArrayList<WaveletParam> returnVal = new ArrayList<WaveletParam>();
-    	double freqInHz;
-    	double cyclesPerWindow;
-    	double taperValue;
+    	int note = maxNote;
+    	double freqInHz = 1.0;
+    	double cyclesPerWindow = 1.0;
+    	double taperValue = 1.0;
     	double ratio = (maxCyclesPerWindow - 1.0) / maxCyclesPerWindow;
     	if((ratio >= 1.0) || (ratio <= 0.0)) {
     		System.out.println("Error: InitFrequencies: invalid ratio: " + "ratio");
     		return null;
     	}
-    	for(freqInHz = upperFreq; freqInHz > centerFreq; freqInHz *= ratio) {
-    		returnVal.add(new WaveletParam(freqInHz, maxCyclesPerWindow));
+    	for(note = maxNote; note > centerNote; note++) {
+    		freqInHz = Math.pow(note, note / noteBase);
+    		returnVal.add(new WaveletParam((float) note, maxCyclesPerWindow));
     	}
     	// start tapering window length
     	double startLogFreq = Math.log(freqInHz) / Math.log(2.0);
     	double currentLogFreq;
-    	for(; freqInHz >= lowerFreq; freqInHz *= ratio) {
+    	for(note = centerNote; note <= minNote; note++) {
+    		freqInHz = Math.pow(note, note / noteBase);
     		currentLogFreq =  Math.log(freqInHz) / Math.log(2.0);
     		taperValue = Math.pow(taperPerOctave, startLogFreq - currentLogFreq);
     		cyclesPerWindow = maxCyclesPerWindow / taperValue;
-    		returnVal.add(new WaveletParam(freqInHz, cyclesPerWindow));
+    		returnVal.add(new WaveletParam((float) note, cyclesPerWindow));
     	}
     	return returnVal;
     }
