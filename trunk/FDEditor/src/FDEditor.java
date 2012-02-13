b7057c3aa022af98749362124636310041d36caa
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2012-02-13 06:29:55 +0000

    This is a significant commit.  Harmonic creation fixed.  Working on synthesis.

--- trunk/FDEditor/src/FDEditor.java
+++ trunk/FDEditor/src/FDEditor.java
@@ -187,8 +187,8 @@ public class FDEditor extends JFrame {
 		if(!test) return;
 		try {
 			addDataInterpolate(new FDData(0, 31.0 * 10.0, 12.0), new FDData(100, 31.0 * 11.0, 0.0), true);
-			addDataInterpolate(new FDData(0, 31.0 * 9.0, 11.0), new FDData(75, 31.0 * 10.0, 0.0), true);
-			addDataInterpolate(new FDData(0, 31.0 * 8.0, 10.0), new FDData(50, 31.0 * 9.0, 0.0), true);
+			addDataInterpolate(new FDData(25, 31.0 * 9.0, 11.0), new FDData(75, 31.0 * 10.0, 0.0), true);
+			addDataInterpolate(new FDData(100, 31.0 * 8.0, 10.0), new FDData(150, 31.0 * 9.0, 0.0), true);
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Data out of bounds", 
 					"FDEditor.initTestData()", 
--- trunk/FDEditor/src/LogLinear.java
+++ trunk/FDEditor/src/LogLinear.java
@@ -1,93 +1,54 @@
 
 class LogLinear {
 
-	boolean logInterpolate = true;
-	int numSamples;
-	int startSample;
-	int endSample;
-	double[] envelope;
-	double sampleRate = 44100.0;
-	public static final double logBase = 2.0;
+	private int numSamples;
+	public double[] envelope;
+	public static double timeToSample = SynthTools.timeToSample;
 	
-	LogLinear(double[] times, double[] values) {
+	LogLinear(Double[] times, Double[] values) {
 		int numTimes = times.length;
 		if(numTimes != values.length) {
 			System.out.println("ERROR: LogLinear: array length mismatch");
 			return;
 		}
-		// double logValue; changed from logarithmic interpolation
-		double envValue;
-		int arrayIndex;
-		double timeIndex;
+		numSamples = (int) Math.round(times[times.length - 1] * timeToSample);
 		int envIndex = 0;
-		double timeStep = 1.0 / sampleRate;
-		startSample = (int) Math.round(times[0] * sampleRate);
-		endSample = (int) Math.round(times[numTimes - 1] * sampleRate);
-		numSamples = endSample - startSample;
-		// System.out.println("LogLinear: endSample = " + endSample + ", numSamples = " + numSamples);
 		envelope = new double[numSamples];
-		// System.out.println("LogLinear: endSample = " + endSample + ", numSamples = " + numSamples);
-		for(arrayIndex = 0; arrayIndex < (numTimes - 1); arrayIndex++) {
-			double lowerValue;
-			double upperValue;
-			double lowerTime = times[arrayIndex];
-			double upperTime = times[arrayIndex + 1];
-			if (logInterpolate) {
-				lowerValue = values[arrayIndex]; // log interpolate
-				upperValue = values[arrayIndex + 1]; // log interpolate
-			} else {
-				lowerValue = Math.pow(logBase, values[arrayIndex]); // linear interpolate
-				upperValue = Math.pow(logBase, values[arrayIndex + 1]); // linear interpolate
-			}		
+		for(int arrayIndex = 0; arrayIndex < (numTimes - 1); arrayIndex++) {
+			int lowerTime = (int) Math.round(times[arrayIndex] * timeToSample);
+			int upperTime = (int) Math.round(times[arrayIndex + 1]* timeToSample);
+			double lowerValue = values[arrayIndex];
+			double upperValue = values[arrayIndex + 1];
 			double slope = (upperValue - lowerValue) / (upperTime - lowerTime);
-			for(timeIndex = lowerTime; timeIndex < upperTime; timeIndex += timeStep) {
-				envValue = lowerValue + (timeIndex - lowerTime) * slope;
-				if (logInterpolate) {
-					envValue = Math.pow(logBase, envValue);
-				}
-				envelope[envIndex] = envValue;
-				envIndex++;
+			for(int timeIndex = lowerTime; timeIndex < upperTime; timeIndex++) {
+				double envValue = lowerValue + (timeIndex - lowerTime) * slope;
+				envelope[envIndex] = Math.pow(FDData.logBase, envValue);
 				if(envIndex == numSamples) {
-					// System.out.println("LogLinear: numSamples reached in loop");
+					//System.out.println("LogLinear: numSamples reached in loop");
 					return;
 				}
 				if((envIndex % 441) == 0) {
-					// System.out.println("LogLinear: envIndex=" + envIndex + ", envValue= " + envValue);
+					//System.out.println("LogLinear: " + envIndex + " | " + envelope[envIndex]);
 				}
+				envIndex++;
 			}	
 		}
-		// System.out.println("LogLinear: envIndex = " + envIndex + ", numSamples = " + numSamples);
-		envelope[numSamples - 1] = 0.0; 
-	}
-	
-	public double getSample(double time) {
-		int sampleIndex = (int) Math.round(time / sampleRate);
-		if((sampleIndex % 441) == 0) {
-			// System.out.println("LogLinear.getSample: time=" + time + ", sample= " + envelope[sampleIndex]);
-		}
-		if((sampleIndex >= startSample) && (sampleIndex < endSample)) {
-			return envelope[sampleIndex];
-		}
-		return 0.0;
+		//System.out.println("LogLinear: envIndex = " + envIndex + ", numSamples = " + numSamples);
 	}
 	
 	public double getSample(int sample) {
 		int sampleIndex = sample;
 		if((sampleIndex % 441) == 0) {
-			// System.out.println("LogLinear.getSample(int): sample=" + sample + ", sample= " + envelope[sampleIndex]);
+			System.out.println("LogLinear.getSample(int): sample=" + sample + ", sample= " + envelope[sampleIndex]);
 		}
-		if((sampleIndex >= startSample) && (sampleIndex < endSample)) {
+		if((sampleIndex >= 0) && (sampleIndex < numSamples)) {
 			return envelope[sampleIndex];
 		}
 		return 0.0;
 	}
 	
-	public int getStartIndex() {
-		return startSample;
+	public int getNumSamples() {
+		return numSamples;
 	}
-	
-	public int getEndIndex() {
-		return endSample;
-	}	
-	
+
 }
