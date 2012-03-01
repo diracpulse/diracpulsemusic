96b6456f66d297492a2877cce314376c111e8a8b
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2012-03-01 06:56:26 +0000

    Adding two files to complete previous revision.

---
+++ trunk/DFTEditor/src/DFTFileInput.java
@@ -0,0 +1,49 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+
+public class DFTFileInput {
+	
+	private DFTEditor parent;
+	
+	public DFTFileInput(DFTEditor parent) {
+		this.parent = parent;
+	}
+	
+	// This function reads from a binary file
+	public static void ReadSelectedFileData(String fileName) {
+		DFTEditor.timeToFreqToSelectedData = new TreeMap<Integer, TreeMap<Integer, FDData>>();
+		DataInputStream in = null;
+	    try {
+	    	in = new DataInputStream(new
+	                BufferedInputStream(new FileInputStream(new String(fileName + ".selected"))));
+		} catch (FileNotFoundException nf) {
+			System.out.println("FileInput: " + fileName + ".[suffix] not found");
+			return;
+		}
+		try {
+			// loop is terminated by EOFException
+			while(true) {
+				int time = in.readInt();
+				int note = in.readInt();
+				float amp = in.readFloat();
+				FDData data = new FDData(time, note, amp);
+				//System.out.println(data);
+				DFTEditor.addSelected(data);
+			}
+		} catch (Exception e) {
+			if(e instanceof EOFException) {
+				System.out.println("Finished reading from: " + fileName);
+			} else {
+				System.out.println("FileInput error: " + e.getMessage());
+			}
+		}
+	}
+	
+}
