b3a7b25dd072633a92e0959b2de9e9a05da7bf57
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2012-04-03 19:05:04 +0000

    Working nested tree map (tested in SimpleSynth)

--- SimpleSynth/src/SimpleSynth.java
+++ SimpleSynth/src/SimpleSynth.java
@@ -3,10 +3,48 @@ import java.lang.*;
 import java.util.*;
 
 public class SimpleSynth {
+	
+	public static Random randomGenerator;
+	public static NestedTreeMap ntm;
+	
+	public static void genArrays() {
+		ntm = new NestedTreeMap();
+		for(int i1 = 0; i1 < 5; i1++) {
+			for(int i2 = 0; i2 < 4; i2++) {
+				for(int i3 = 0; i3 < 3; i3++) {
+					for(int i4 = 0; i4 < 2; i4++) {
+						int[] array = new int[]{i1,i2,i3,i4};
+						ntm.addArray(array);
+					}
+				}
+			}
+		}
+		printArrays();
+	}
+	
+	public static void printArrays() {
+		int loopIndex = 0;
+		for(int index = 0; index < 1200; index++) {
+			ArrayList<Integer> values = ntm.getRandomArray();
+			if(values.size() < 4) continue;
+			System.out.print(loopIndex + ": ");
+			for(int value: values) {
+				System.out.print(value + " ");
+			}
+			System.out.println();
+			loopIndex++;
+		}
+	}
 
-	public static void main(String[] args) {
+	public static void main2(String[] args) {
 		Chords c = new Chords();
 		c.playChords();
+		System.exit(0);
+	}
+	
+	public static void main(String[] args) {
+		randomGenerator = new Random();
+		genArrays();
 		System.exit(0);
 	}
 	
