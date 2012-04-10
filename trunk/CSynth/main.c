9d2c82fe12d34b8829441cae028c9c1a6f17976a
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2012-04-10 03:38:07 +0000

    More work towards synthesizing harmonics using C executable.

--- trunk/CSynth/main.c
+++ trunk/CSynth/main.c
@@ -42,11 +42,11 @@ void LoadDataFromFile(FILE *stream, int offset, int index) {
 	fread((void *) &noteIn, sizeof(int), 1, stream);
 	fread((void *) &logAmplitudeIn, sizeof(double), 1, stream);
 	fread((void *) &harmonicIDIn, sizeof(long), 1, stream);
-	FDDataArray[index].time = timeIn;
-	FDDataArray[index].note = noteIn;
-	FDDataArray[index].logAmplitude = logAmplitudeIn;
-	FDDataArray[index].harmonicID = harmonicIDIn;
-	dAverageNote += (double) note;
+	FDDataArray[index].time = *timeIn;
+	FDDataArray[index].note = *noteIn;
+	FDDataArray[index].logAmplitude = *logAmplitudeIn;
+	FDDataArray[index].harmonicID = *harmonicIDIn;
+	dAverageNote += (double) FDDataArray[index].note;
 }
 
 void WriteDataToFile(FILE *stream) {
@@ -59,7 +59,7 @@ int ReadAllData(FILE *stream) {
 	int offset;
 	int index = 0;
 	for(offset = 0; offset < inputFileLength; offset += (4 + 4 + 8 + 8)) {
-		LoadDataFromFile(stream, offset);
+		LoadDataFromFile(stream, offset, index);
 		index++;
 	}
 	numFDData = index;
@@ -68,44 +68,35 @@ int ReadAllData(FILE *stream) {
 
 void Normalize() {
 	double maxAmplitude = 0.0;
-	for(int arrayIndex = 0; arrayIndex < maxSample; arrayIndex++) {
+	int arrayIndex;
+	for(arrayIndex = 0; arrayIndex < maxSample; arrayIndex++) {
 		if(sampleArray[arrayIndex] > maxAmplitude) maxAmplitude = sampleArray[arrayIndex];
 	}
 	if(maxAmplitude == 0) {
 		printf("No Sample Data\n");
 		return;
 	}
-	for(int arrayIndex = 0; arrayIndex < maxSample; arrayIndex++) {
+	for(arrayIndex = 0; arrayIndex < maxSample; arrayIndex++) {
 		sampleArray[arrayIndex] = (sampleArray[arrayIndex] / maxAmplitude) *  max16BitAmplitude;
 	}
 }
 
-void SynthAllData() {
-	for(int arrayIndex = 0; arrayIndex < MAXSAMPLES; arrayIndex++) sampleArray[arrayIndex] = 0.0;
-	int startIndex = 0;
-	int endIndex = 0;
-	while(endIndex < numFDData) {
-		long harmonicID = FDDataArray[startIndex].harmonicID;
-		while(FDDataArray[endIndex].harmonicID == harmonicID) endIndex++;
-		SynthHarmonicFlat(startIndex, endIndex - 1);
-		startIndex = endIndex;
-	}
-}
-
 void SynthHarmonicFlat(int startIndex, int endIndex) {
+	int arrayIndex;
+	int timeIndex;
 	int maxArrayIndex = numFDData;
 	double currentPhase = 0.0;
 	int averageNote = round(dAverageNote / (double) numFDData);
 	double averageFreq = pow(2.0, (double) averageNote / notesPerOctave);
 	double deltaPhase = (averageFreq / sampleRate) * twoPI;
-	for(int arrayIndex = startIndex; arrayIndex < endIndex - 1; arrayIndex++) {
+	for(arrayIndex = startIndex; arrayIndex < endIndex - 1; arrayIndex++) {
 		int lowerTime = round(FDDataArray[arrayIndex].time * timeToSample);
 		int upperTime = round(FDDataArray[arrayIndex + 1].time * timeToSample);
 		if(upperTime > maxSample) maxSample = upperTime;
-		double lowerLogAmplitude = FDDataArray[arrayIndex].logAmplitude();
-		double upperLogAmplitude = FDDataArray[arrayIndex + 1].logAmplitude();
+		double lowerLogAmplitude = FDDataArray[arrayIndex].logAmplitude;
+		double upperLogAmplitude = FDDataArray[arrayIndex + 1].logAmplitude;
 		double slope = (upperLogAmplitude - lowerLogAmplitude) / (upperTime - lowerTime);
-		for(int timeIndex = lowerTime; timeIndex < upperTime; timeIndex++) {
+		for(timeIndex = lowerTime; timeIndex < upperTime; timeIndex++) {
 			double logAmplitude = lowerLogAmplitude + (timeIndex - lowerTime) * slope;
 			double amplitude = exp(logAmplitude * log2val);
 			sampleArray[timeIndex] += sin(currentPhase) * amplitude;
@@ -113,6 +104,23 @@ void SynthHarmonicFlat(int startIndex, int endIndex) {
 		}
 	}
 }
+
+
+void SynthAllData() {
+	int arrayIndex;
+	int endIndexArg;
+	for(arrayIndex = 0; arrayIndex < MAXSAMPLES; arrayIndex++) sampleArray[arrayIndex] = 0.0;
+	int startIndex = 0;
+	int endIndex = 0;
+	while(endIndex < numFDData) {
+		long harmonicID = FDDataArray[startIndex].harmonicID;
+		while(FDDataArray[endIndex].harmonicID == harmonicID) endIndex++;
+		endIndexArg = endIndex - 1;
+		SynthHarmonicFlat(startIndex, endIndexArg);
+		startIndex = endIndex;
+	}
+}
+
 
 void speedTest() {
 	register double result = 0.0;
