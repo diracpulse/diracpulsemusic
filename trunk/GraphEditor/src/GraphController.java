2b8f7a6dc7a571e2bdb5fb48d941ddf2c0e36918
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2012-04-19 03:54:22 +0000

    GraphEditor: selectable harmonics in amplitude view.

--- trunk/GraphEditor/src/GraphController.java
+++ trunk/GraphEditor/src/GraphController.java
@@ -33,10 +33,27 @@ public class GraphController implements MouseListener, ActionListener {
 	    	return;
 	    }
 	    if(e.isShiftDown()) {
+	    	if(y < GraphEditor.upperOffset) return;
 	    	if(GraphView.yView == GraphView.YView.AMPLITUDE) GraphEditor.zoomInAmplitude(y);
 	    	if(GraphView.yView == GraphView.YView.FREQUENCY) GraphEditor.zoomInFrequency(y);
 	    	return;
 	    }
+	    if(GraphView.yView == GraphView.YView.AMPLITUDE) {
+	    	int time = GraphUtils.screenXToTime(x);
+	    	System.out.println(GraphUtils.screenYToValue(y));
+	    	int logAmplitudeTimes10 = (int) Math.round(GraphUtils.screenYToValue(y) * 10.0);
+	    	for(int testTime = time - 2; testTime < time + 3; testTime++) {
+	    		if(GraphEditor.timeToLogAmplitudeTimes10ToHarmonicID.containsKey(testTime)) {
+	    			if(GraphEditor.timeToLogAmplitudeTimes10ToHarmonicID.get(testTime).containsKey(logAmplitudeTimes10)) {
+	    				long harmonicID = GraphEditor.timeToLogAmplitudeTimes10ToHarmonicID.get(testTime).get(logAmplitudeTimes10);
+	    				GraphEditor.selectedHarmonicIDs.add(harmonicID);
+	    				view.repaint();
+	    				return;
+	    			}
+	    		}
+	    	}
+	    }
+	    
 	}
 	
 	public void mouseClicked(MouseEvent e){
