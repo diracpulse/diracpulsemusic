5a1d596631cc271ad734212cf0e608b4fd40ab50
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2012-10-30 02:28:41 +0000

    DFTEditor: Updates / bug fixes with selection in GraphEditor.  Start of linkage between FDEditor and GraphEditor.

--- DFTEditor/src/GraphUtils.java
+++ DFTEditor/src/GraphUtils.java
@@ -1,9 +1,39 @@
 
 public class GraphUtils {
 	
+	private static boolean isChannelVisible(Harmonic harmonic) {
+		if(harmonic.getChannel() == 0) {
+			if(GraphEditor.currentChannel == GraphEditor.Channel.MONO) return true;
+			return false;			
+		}
+		if(harmonic.getChannel() == 1) {
+			if(GraphEditor.currentChannel == GraphEditor.Channel.LEFT) return true;
+			if(GraphEditor.currentChannel == GraphEditor.Channel.STEREO) return true;
+			return false;
+		}
+		if(harmonic.getChannel() == 2) {
+			if(GraphEditor.currentChannel == GraphEditor.Channel.RIGHT) return true;
+			if(GraphEditor.currentChannel == GraphEditor.Channel.STEREO) return true;
+			return false;
+		}
+		System.out.println("DFTView.isHarmonicVisible: Unknown channel");
+		return false;
+	}
+
+	public static boolean isHarmonicVisible(Harmonic harmonic) {
+		if(!isChannelVisible(harmonic)) return false;
+		if(!harmonic.isSynthesized()) return false;
+		if(harmonic.getLength() < GraphEditor.minHarmonicLength) return false;
+		if(harmonic.getAverageNote() < GraphEditor.minViewNote || harmonic.getAverageNote() > GraphEditor.maxViewNote) return false;
+		if(harmonic.getMaxLogAmplitude() < GraphEditor.minViewLogAmplitude) return false;
+		if(harmonic.getMaxLogAmplitude() > GraphEditor.maxViewLogAmplitude) return false;
+		return true;
+	}
+	
+	
 	public static double screenXToValue(double x) {
 		if(x < 0) x = 0;
-		if(x > GraphEditor.view.getHeight()) x = GraphEditor.view.getHeight();
+		if(x > GraphEditor.view.getWidth()) x = GraphEditor.view.getWidth();
 		double pixelsPerValue = 0.0;
 		if(GraphView.xView == GraphView.XView.TIME) pixelsPerValue = (double) GraphEditor.view.getWidth() / (GraphEditor.maxViewTime - GraphEditor.minViewTime);
 		if(GraphView.xView == GraphView.XView.FREQUENCY) pixelsPerValue = (double) GraphEditor.view.getWidth() / (GraphEditor.maxViewNote - GraphEditor.minViewNote);
