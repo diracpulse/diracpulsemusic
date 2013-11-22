9e066fa681b4ba38d8e7cbd8031ac0f40d17ef6c
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2013-11-22 08:54:05 +0000

    DFTEditor: Implemented psychoacoustical masking.  Still issues with lots of harmonics at low frequencies (which are unaffected by masking due to their similar amplitude).

--- DFTEditor/src/DFT2.java
+++ DFTEditor/src/DFT2.java
@@ -35,7 +35,7 @@ public class DFT2 {
 	//private static double maxFreqHz = samplingRate / 2.0;
 	//private static double minFreqHz = 20.0;
 	private static double maxWindowLength = 44100 / 5;
-	private static double alpha = 5.0;
+	private static double alpha = 1.0;
 	public static double midFreq = 0.0;
 	public static double bassFreq = 640.0;
 	
@@ -447,8 +447,9 @@ public class DFT2 {
 		}
 	}
 	
+
 	static void FileDFTMatrix(String fileName) {
-		disentangleWithMatrix();
+		//disentangleWithMatrix();
 		double samplesPerStep = SynthTools.sampleRate / (1000.0 / FDData.timeStepInMillis);
 		int maxCenterIndex = LoadSamplesFromFile(fileName);
 		int maxTime = (int) Math.floor(maxCenterIndex / samplesPerStep);
--- DFTEditor/src/FastSynth.java
+++ DFTEditor/src/FastSynth.java
@@ -13,10 +13,10 @@ public class FastSynth {
 	
 	public static double[] synthHarmonicsLinear(FDData.Channel channel, ArrayList<Harmonic> harmonics) {
 		initSharedPCMData(channel, harmonics);
-		synthBackgroundNoise(channel);
+		//synthBackgroundNoise(channel);
 		for(Harmonic harmonic: harmonics) {
 			if(harmonic.getChannel() != channel) continue; 
-			//synthHarmonicLinear(harmonic);
+			synthHarmonicLinear(harmonic);
 		}
 		return sharedPCMData;
 	}
