36e87851a544d20e11babed815261900da18e1e4
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2013-12-01 08:04:20 +0000

    DFTEditor: Finished turning off FDEditor and GraphEditor

--- DFTEditor/src/FDData.java
+++ DFTEditor/src/FDData.java
@@ -3,7 +3,7 @@
 public class FDData {
 	
 	public static int timeStepInMillis = 5; // time in secs = this.time * timeStepInMillis / 1000
-	public static int noteBase = 144; // freq in Hz = 2.0^((this.note + this.noteFraction) / noteBase)
+	public static int noteBase = 53; // freq in Hz = 2.0^((this.note + this.noteFraction) / noteBase)
 	public static final double logBase = 2.0; // amplitude = logBase ^ logAmplitude
 	public static final int bitDepth = 16;
 	
