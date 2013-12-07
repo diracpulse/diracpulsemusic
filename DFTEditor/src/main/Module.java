96004268ceeb6e4e6131699b5af749b502a61a54
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2013-12-07 09:44:05 +0000

    DFTEditor: Added view and controller for Modules

--- DFTEditor/src/main/Module.java
+++ DFTEditor/src/main/Module.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.awt.Graphics;
+
 public interface Module {
 	
 	public enum InputType {
@@ -8,5 +10,7 @@ public interface Module {
 	}
 	
 	abstract void mousePressed(int x, int y);
-	abstract void draw(int startX, int startY);
+	abstract void draw(Graphics g, int startX, int startY);
+	abstract int getWidth();
+	abstract int getHeight();
 }
--- DFTEditor/src/main/ModuleView.java
+++ DFTEditor/src/main/ModuleView.java
@@ -1,17 +1,29 @@
 package main;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
+
 import javax.swing.JComponent;
+
+import main.ModuleEditor.ModuleScreenInfo;
 
 
 public class ModuleView extends JComponent {
 	
 	private static final long serialVersionUID = -443010057744196001L;
 
+	private ArrayList<ModuleScreenInfo> moduleScreenInfoArray = null;
+	
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		return;
-    	
+		for(ModuleScreenInfo moduleScreenInfo: moduleScreenInfoArray) {
+			moduleScreenInfo.module.draw(g, moduleScreenInfo.dimensions.x, moduleScreenInfo.dimensions.y);
+		}
     }
+	
+	public void updateModuleScreenInfo(ArrayList<ModuleScreenInfo> moduleScreenInfoArray) {
+		this.moduleScreenInfoArray = moduleScreenInfoArray;
+	}
+
 	
 }
