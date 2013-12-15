62113c01cad14a7c1a479b266f537af8b1d53b18
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2013-12-15 08:31:28 +0000

    DFTEditor: Sequencer is working, needs to be integrated with modules.

--- DFTEditor/src/main/Sequencer.java
+++ DFTEditor/src/main/Sequencer.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.TreeMap;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -16,18 +17,34 @@ import javax.swing.JToolBar;
 
 public class Sequencer extends JPanel {
 
-	private static final long serialVersionUID = 6252327634736973395L;
-
+	private static final long serialVersionUID = 3138005743637187863L;
+	
 	MultiWindow parent;
 	SequencerView view;
 	SequencerController controller;
 	ArrayList<ModuleEditor> moduleEditors = null;
+	public JScrollPane scrollPane;
 	private JToolBar navigationBar = null;
-	public static final int columnWidth = 150;
-	public static final int scrollableWidth = columnWidth * 16;
-	public static final int scrollableHeight = columnWidth * 8;
+	public static int noteBase = 53;
+	public static double minFreq = 256.0;
+	public static int noteHeight = 13;
+	public static int numPercussion = 3;
+	public static double bpm = 120;
+	public static int maxBeats = 20;
+	public static int pixelsPerBeat = 128;
+	public static int totalPixels = maxBeats * pixelsPerBeat;
+	public static int divisionsPerBeat = 8;
+	public static int pixelsPerDivision = pixelsPerBeat / divisionsPerBeat;
+	public static double secondsPerBeat = 60.0 / bpm;
+	public static double maxTimeInSeconds = maxBeats * secondsPerBeat;
+	public static int leftDigits = 9;
+	public static int scrollableWidth = totalPixels + SequencerUtils.digitWidth * leftDigits;
+	public static double secondsPerPixel = secondsPerBeat / pixelsPerBeat;
+	public static int scrollableHeight = (noteBase + 3) * noteHeight;
+	public ArrayList<double[]> freqRatiosAtTime;
+	public ArrayList<MultiWindow.ModuleEditorInfo> moduleInfo;
+	public int currentModuleIndex = 0;
 	
-
 	public void addNavigationButton(String buttonText) {
 		JButton button = new JButton(buttonText);
 		button.addActionListener((ActionListener) controller);
@@ -41,6 +58,7 @@ public class Sequencer extends JPanel {
         addNavigationButton("DFT");
         addNavigationButton("Load");
         addNavigationButton("Save");
+        addNavigationButton("Get Module");
     	return navigationBar;
 	}
 
@@ -55,9 +73,19 @@ public class Sequencer extends JPanel {
         view.addMouseListener(controller);
         view.addMouseMotionListener(controller);
         view.setPreferredSize(new Dimension(scrollableWidth, scrollableHeight));
-        JScrollPane scrollPane = new JScrollPane(view);
+        scrollPane = new JScrollPane(view);
         scrollPane.setSize(800, 600);
         add(scrollPane, BorderLayout.CENTER);
+        freqRatiosAtTime = new ArrayList<double[]>();
+        moduleInfo = new ArrayList<MultiWindow.ModuleEditorInfo>();
+        for(MultiWindow.ModuleEditorInfo info: parent.moduleEditorInfo) {
+        	freqRatiosAtTime.add(new double[maxBeats * pixelsPerBeat]);
+        	double[] freqRatioAtTime = freqRatiosAtTime.get(freqRatiosAtTime.size() - 1);
+        	for(int time = 0; time < freqRatioAtTime.length; time++) {
+        		freqRatioAtTime[time] = -1.0;
+        	}
+        	moduleInfo.add(info);
+        }
     }
 
 }
--- DFTEditor/src/main/SequencerView.java
+++ DFTEditor/src/main/SequencerView.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -15,7 +16,86 @@ public class SequencerView extends JPanel {
 	
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
+		drawLeftFreqs(g);
+		drawTimeGrid(g);
+		drawSequencerData(g);
+	}
+	
+	public void drawLeftFreqs(Graphics g) {
+		int yOffset = (Sequencer.noteHeight - SequencerUtils.digitHeight) / 2;
+		int y = yOffset;
+		int x = parent.scrollPane.getVisibleRect().x;
+		Color white = new Color(0.0f, 0.0f, 0.0f);
+		Color black = new Color(1.0f, 1.0f, 1.0f);
+		for (int note = Sequencer.noteBase - 1; note >= -3; note--) {
+			if(note >= 0) {
+				int freq = (int) Math.round(Math.pow(2.0, 1.0 * note / Sequencer.noteBase) * Sequencer.minFreq);
+				SequencerUtils.DrawIntegerHorizontal(g, white, black, x, y, 4,
+						freq);
+				SequencerUtils.DrawIntegerHorizontal(g, white, black, x + 6 * SequencerUtils.digitWidth, y, 1,
+						note);
+				int lineY = y + SequencerUtils.digitHeight + yOffset;
+				g.setColor(Color.WHITE);
+				g.drawLine(0, lineY, Sequencer.scrollableWidth, lineY);
+				y += Sequencer.noteHeight;
+			} else {
+				SequencerUtils.DrawIntegerHorizontal(g, white, black, x, y, 4,
+						Math.abs(note));
+				SequencerUtils.DrawIntegerHorizontal(g, white, black, x + 6 * SequencerUtils.digitWidth, y, 1,
+						Math.abs(note));
+				int lineY = y + SequencerUtils.digitHeight + yOffset;
+				g.setColor(Color.WHITE);
+				g.drawLine(0, lineY, Sequencer.scrollableWidth, lineY);
+				y += Sequencer.noteHeight;
+			}
+			// System.out.println("drawUpperTimes " + intVal + " " +
+			// decimalVal);
+		}
+	}
+	
+	public void drawTimeGrid(Graphics g) {
+		int xOffset = SequencerUtils.digitWidth * Sequencer.leftDigits;
+		g.setColor(Color.DARK_GRAY);
+		for(int x = xOffset; x < Sequencer.scrollableWidth; x += Sequencer.pixelsPerDivision) {
+			g.drawLine(x,  0, x, Sequencer.scrollableHeight);
+		}
+		g.setColor(Color.GRAY);
+		for(int x = xOffset; x < Sequencer.scrollableWidth; x += Sequencer.pixelsPerBeat) {
+			g.drawLine(x,  0, x, Sequencer.scrollableHeight);
+		}
+	}
+	
+	public void drawSequencerData(Graphics g) {
+		int yOffset = (Sequencer.noteHeight - SequencerUtils.digitHeight) / 2;
+		int index = 0;
+		for(double[] freqRatio: parent.freqRatiosAtTime) {
+			g.setColor(parent.moduleInfo.get(index).getColor());
+			for(int time = 0; time < parent.totalPixels; time++) {
+				if(freqRatio[time] >= 0.0) {
+					int note = (int) Math.round(Math.log(freqRatio[time]) / Math.log(2.0) * Sequencer.noteBase);
+					int y0 = (Sequencer.noteBase - note - 1) * Sequencer.noteHeight;
+					int y1 = (Sequencer.noteBase - note) * Sequencer.noteHeight - yOffset;
+					int x = time + Sequencer.leftDigits * SequencerUtils.digitWidth;
+					g.drawLine(x, y0, x, y1);
+				}
+			}
+			index++;
+		}
+	}
+	
+	public int getNote(int y) {
+		int note = Sequencer.noteBase - 1 - y / Sequencer.noteHeight;
+		return note;
+	}
+
+	public int getTimeInBeats(int x) {
+		int time = (int)  Math.round(((x - SequencerUtils.digitWidth * Sequencer.leftDigits) / Sequencer.pixelsPerBeat));
+		return time;
+	}
+	
+	public int getTimeInDivisions(int x) {
+		int time = (int)  Math.round(((x - SequencerUtils.digitWidth * Sequencer.leftDigits) / Sequencer.pixelsPerDivision));
+		return time;
 	}
 	
 }
