db5d8e0ed56801bebcb7b250ed19f8b806dc8525
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2014-11-29 10:36:07 +0000

    ArduinoSynth: Four oscillators working (see previous commit for TB303 like single oscillator)

--- ArduinoSynth/src/Synth.cpp
+++ ArduinoSynth/src/Synth.cpp
@@ -1,15 +1,6 @@
-/*
-Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  This example code is in the public domain.
- */
-
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-
 
 #include "Arduino.h"
+/*
 #include "binary.h"
 #include "Client.h"
 #include "HardwareSerial.h"
@@ -28,165 +19,286 @@ Blink
 #include "WCharacter.h"
 #include "wiring_private.h"
 #include "WString.h"
-#include <avr/pgmspace.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-
+*/
 // INO Starts HERE
 
-#include "eeprom.h"
-#include <TimerOne.h>
-#include <MsTimer2.h>
+//#include "eeprom.h"
 
-void timer1ISR();
-void timer2ISR();
-
-const float octave[] = {63.05, 126.1, 252.2};
+const float octave[] = {63.05, 126.1, 252.2, 504.4};
 const float scale[] = {1.0, 17.0 / 16.0, 9.0 / 8.0, 6.0 / 5.0, 5.0 / 4.0, 4.0 / 3.0, 11.0 / 8.0, 3.0 / 2.0, 13.0 / 8.0, 27.0 / 16.0, 7.0 / 4.0, 15.0 / 8.0};
-byte octaveIndex = 2;
-byte pattern[] = {1, 3, 6, 6, 1, 8, 11, 1, 12};
-byte accentArray[]  = {1, 0, 1, 0, 0, 1, 0,  0, 12};
-byte slideArray[]   = {0, 0, 1, 0, 0, 1, 0,  1, 12};
-byte patternIndex = 0;
-const float slideTimeInMicros = 25.0 * 1000.0;
-float accentApexInMicros = 50.0 * 1000.0;
-float accentApexScalar = 1.5;
-float beatLengthInMicros = 1000.0 * 1000.0;
-volatile float microsElapsed = 0;
-float startSlideTimeInMicros = 0;
-float bpm = 180;
-byte currentNote = 0;
-byte nextNote = 0;
-float saveCurrentMicrosPerPulse;
-float currentMicrosPerPulse;
-float nextMicrosPerPulse;
-float deltaMicrosPerPulse;
-bool pulsePhase = false;
-bool silence = false;
-bool slide = false;
-bool startSlide = true;
-bool firstBeat = true;
-bool newFreq = false;
-bool accent = true;
-bool accentUpward = true;
-bool accentDownward = true;
-bool accentToggle = true;
-byte nextBeat = 1;
-byte nextPulse = 1;
+int scaleIndex = 0;
+// Arrays not used for faster speed
+// Set to false when OSCxFullCount is being updated in case of interrupt
+volatile bool timer2Loop = false;
+volatile bool OSC1CountUpdated = true;
+volatile bool OSC2CountUpdated = true;
+volatile bool OSC3CountUpdated = true;
+volatile bool OSC4CountUpdated = true;
+volatile int OSC1CountOld = 0;
+volatile int OSC2CountOld = 0;
+volatile int OSC3CountOld = 0;
+volatile int OSC4CountOld = 0;
+volatile int OSC1Count = 0;
+volatile int OSC2Count = 0;
+volatile int OSC3Count = 0;
+volatile int OSC4Count = 0;
+volatile int OSC1CountNext = 0;
+volatile int OSC2CountNext = 0;
+volatile int OSC3CountNext = 0;
+volatile int OSC4CountNext = 0;
+volatile bool OSC1Phase = false;
+volatile bool OSC2Phase = false;
+volatile bool OSC3Phase = false;
+volatile bool OSC4Phase = false;
+bool OSC1Active = false;
+bool OSC2Active = false;
+bool OSC3Active = false;
+bool OSC4Active = false;
+#define OSC1PIN 2
+#define OSC2PIN 3
+#define OSC3PIN 4
+#define OSC4PIN 5
+volatile bool nextBeat = false;
+volatile bool timer2InterruptInProgress = false;
+void handleTimer2Interrupt();
+void initTimer1(long microseconds);
+void initTimer2();
+void setFrequency1(float freq);
+void setFrequency2(float freq);
+void setFrequency3(float freq);
+void setFrequency4(float freq);
 
 void setup()
 {
   pinMode(13, OUTPUT);
   DDRD = DDRD | B11111100;  // sets pins 2 to 7 as outputs
-  beatLengthInMicros = 60.0 / bpm * 1000000.0;
-  int beatLengthInMillis = round(beatLengthInMicros / 1000.0);
-  MsTimer2::set(beatLengthInMillis, timer2ISR); // 500ms period
-  MsTimer2::start();
+  initTimer1(8000000);
+  initTimer2();
+  scaleIndex = 0;
 }
 
 void loop() {
-	if(nextBeat == 1) {
-		currentNote = pattern[(int) patternIndex];
-		slide = slideArray[patternIndex];
-		accent = accentArray[patternIndex];
-		if(currentNote > 11) {
-			patternIndex = 0;
-			currentNote = pattern[(int) patternIndex];
-			slide = slideArray[patternIndex];
-			accent = accentArray[patternIndex];
-		}
-		patternIndex++;
-		nextNote = pattern[(int) patternIndex];
-		if(nextNote > 11) {
-			nextNote = pattern[0];
-		}
-		float freqInHz = octave[octaveIndex] * scale[currentNote];
-		float newCurrentMicrosPerPulse = 500000.0 / freqInHz;
-		freqInHz = octave[octaveIndex] * scale[nextNote];
-		float newNextMicrosPerPulse = 500000.0 / freqInHz;
-		microsElapsed = 0.0;
-		startSlideTimeInMicros = beatLengthInMicros - slideTimeInMicros;
-		startSlide = true;
-		silence = false;
-		accentUpward = true;
-		accentDownward = true;
-		accentToggle = true;
-		currentMicrosPerPulse = newCurrentMicrosPerPulse;
-		nextMicrosPerPulse = newNextMicrosPerPulse;
-		if(firstBeat) {
-			Timer1.initialize(round(currentMicrosPerPulse));
-			Timer1.attachInterrupt(timer1ISR); // attach the service routine here
-			firstBeat = false;
-		} else {
-			Timer1.setPeriod(round(currentMicrosPerPulse));
-		}
-		nextBeat = 0;
+	if(nextBeat) {
+		nextBeat = false;
+		setFrequency1(octave[0] * scale[scaleIndex]);
+		setFrequency2(octave[1] * scale[scaleIndex]);
+		setFrequency3(octave[2] * scale[scaleIndex]);
+		setFrequency4(octave[3] * scale[scaleIndex]);
+		scaleIndex = (scaleIndex + 1) % 12;
 	}
 }
 
-void timer1ISR()
+// Timer 1 Code:
+
+void initTimer1(long microseconds) {
+	  TCCR1A = 0;                 // clear control register A
+	  TCCR1B = _BV(WGM13);        // set mode 8: phase and frequency correct pwm, stop the timer
+	  long cycles = 65535;                                // the counter runs backwards after TOP, interrupt is at BOTTOM so divide microseconds by 2
+	  unsigned char oldSREG = SREG;
+	  cli();							// Disable interrupts for 16 bit register access
+	  ICR1 = cycles;                    // ICR1 is TOP in p & f correct pwm mode
+	  SREG = oldSREG;
+	  TIMSK1 = _BV(TOIE1);
+	  TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
+	  TCCR1B |= _BV(CS11) | _BV(CS10);  // prescale by /64
+	  sei();
+}
+
+ISR(TIMER1_OVF_vect)          // interrupt service routine that wraps a user defined function supplied by attachInterrupt
 {
-	if(silence) {
-		digitalWrite(7, 0);
-		return;
-	}
-	pulsePhase = !pulsePhase;
-	if(pulsePhase) {
-		digitalWrite(7, 1);
-	} else {
-		digitalWrite(7, 0);
-	}
-	if(nextBeat == 1) return; // variables in flux
-	if(nextPulse > 0) {
-		nextPulse--;
-		return;
-	}
-	nextPulse = 16;
-	microsElapsed += currentMicrosPerPulse;
-	if(accent && accentToggle) {
-		if(accentUpward) {
-			saveCurrentMicrosPerPulse = currentMicrosPerPulse;
-			float averageMicrosPerPulse = (currentMicrosPerPulse / accentApexScalar + currentMicrosPerPulse) / 2.0;
-			float numPulsesInSlide = accentApexInMicros / averageMicrosPerPulse;
-			deltaMicrosPerPulse = (currentMicrosPerPulse / accentApexScalar - currentMicrosPerPulse) / numPulsesInSlide;
-			accentUpward = false;
-		} else {
-			if(microsElapsed >= accentApexInMicros && accentDownward) {
-				float averageMicrosPerPulse = (saveCurrentMicrosPerPulse / accentApexScalar + saveCurrentMicrosPerPulse) / 2.0;
-				float numPulsesInSlide = accentApexInMicros / averageMicrosPerPulse;
-				deltaMicrosPerPulse = (saveCurrentMicrosPerPulse - saveCurrentMicrosPerPulse / accentApexScalar) / numPulsesInSlide;
-				accentDownward = false;
-			}
-			if(microsElapsed >= accentApexInMicros * 2) {
-				currentMicrosPerPulse = saveCurrentMicrosPerPulse;
-				deltaMicrosPerPulse = 0;
-				accentToggle = false;
-			}
-		}
-		currentMicrosPerPulse += deltaMicrosPerPulse;
-		Timer1.setPeriod(round(currentMicrosPerPulse));
-	}
-	if(microsElapsed >= startSlideTimeInMicros) {
-		if(slide) {
-			if(startSlide) {
-				float averageMicrosPerPulse = (currentMicrosPerPulse + nextMicrosPerPulse) / 2.0;
-				float numPulsesInSlide = slideTimeInMicros / averageMicrosPerPulse;
-				deltaMicrosPerPulse = (nextMicrosPerPulse - currentMicrosPerPulse) / numPulsesInSlide;
-				Timer1.setPeriod(round(currentMicrosPerPulse));
-				startSlide = false;
+	nextBeat = true;
+}
+
+// Timer 2 Code:
+
+void setFrequency1(float freqInHz) {
+	long cycles = round((1.0 / freqInHz) * 16000000.0);
+	cycles >>= 5;
+	OSC1CountUpdated = false;
+	OSC1CountNext = cycles;
+	OSC1CountUpdated = true;
+	OSC1CountOld = cycles;
+	OSC1Active = true;
+}
+
+void setFrequency2(float freqInHz) {
+	long cycles = round((1.0 / freqInHz) * 16000000.0);
+	cycles >>= 5;
+	OSC2CountUpdated = false;
+	OSC2CountNext = cycles;
+	OSC2CountUpdated = true;
+	OSC2CountOld = cycles;
+	OSC2Active = true;
+}
+
+void setFrequency3(float freqInHz) {
+	long cycles = round((1.0 / freqInHz) * 16000000.0);
+	cycles >>= 5;
+	OSC3CountUpdated = false;
+	OSC3CountNext = cycles;
+	OSC3CountUpdated = true;
+	OSC3CountOld = cycles;
+	OSC3Active = true;
+}
+
+void setFrequency4(float freqInHz) {
+	long cycles = round((1.0 / freqInHz) * 16000000.0);
+	cycles >>= 5;
+	OSC4CountUpdated = false;
+	OSC4CountNext = cycles;
+	OSC4CountUpdated = true;
+	OSC4CountOld = cycles;
+	OSC4Active = true;
+}
+
+void initTimer2() {
+
+	/* First disable the timer overflow interrupt while we're configuring */
+	TIMSK2 &= ~(1<<TOIE2);
+	/* Configure timer2 in normal mode (pure counting, no PWM etc.) */
+	TCCR2A &= ~((1<<WGM21) | (1<<WGM20));
+	TCCR2B &= ~(1<<WGM22);
+	/* Select clock source: internal I/O clock */
+	ASSR &= ~(1<<AS2);
+	/* Disable Compare Match A interrupt enable (only want overflow) */
+	TIMSK2 &= ~(1<<OCIE2A);
+	/* Now configure the prescaler to CPU clock divided by 64 */
+	TCCR2B |= (1<<CS21) | (1<<CS20); // | (1<<CS20); // Set Bit
+	TCCR2B &= ~(1<<CS22); // Clear Bit
+	/* Finally load end enable the timer */
+	TCNT2 = 0;
+	TIMSK2 |= (1<<TOIE2);
+}
+
+// Called when timer 2 overflows
+ISR(TIMER2_OVF_vect) {
+	OSC1Count -= 256;
+	OSC2Count -= 256;
+	OSC3Count -= 256;
+	OSC4Count -= 256;
+	handleTimer2Interrupt();
+}
+
+void handleTimer2Interrupt() {
+	unsigned char tcnt2 = TCNT2;
+	unsigned char tcnt2plus1 = TCNT2;
+	unsigned char oldTCNT2 = tcnt2;
+	unsigned char unfinished = B00001111;
+	while(tcnt2 <= 253) {
+		unsigned char tcnt2plus1 = tcnt2 + 2;
+		signed int subtractValue = 256 - tcnt2;
+		if(OSC1Count <= tcnt2plus1) {
+			OSC1Phase = !OSC1Phase;
+			if(OSC1Phase) {
+				bitSet(PORTD, OSC1PIN);
 			} else {
-				currentMicrosPerPulse += deltaMicrosPerPulse;
-				Timer1.setPeriod(round(currentMicrosPerPulse));
+				bitClear(PORTD, OSC1PIN);
 			}
+			if(OSC1CountUpdated) {
+				OSC1Count = OSC1CountNext - subtractValue; // Account for current TCNT != 0
+			} else {
+				OSC1Count = OSC1CountOld - subtractValue; // Account for current TCNT != 0
+			}
+			unfinished &= B11111110;
+		}
+		if(OSC2Count <= tcnt2plus1) {
+			OSC2Phase = !OSC2Phase;
+			if(OSC2Phase) {
+				bitSet(PORTD, OSC2PIN);
+			} else {
+				bitClear(PORTD, OSC2PIN);
+			}
+			if(OSC2CountUpdated) {
+				OSC2Count = OSC2CountNext - subtractValue; // Account for current TCNT != 0
+			} else {
+				OSC2Count = OSC2CountOld - subtractValue; // Account for current TCNT != 0
+			}
+			unfinished &= B11111101;
+		}
+		if(OSC3Count <= tcnt2plus1) {
+			OSC3Phase = !OSC3Phase;
+			if(OSC3Phase) {
+				bitSet(PORTD, OSC3PIN);
+			} else {
+				bitClear(PORTD, OSC3PIN);
+			}
+			if(OSC3CountUpdated) {
+				OSC3Count = OSC3CountNext - subtractValue; // Account for current TCNT != 0
+			} else {
+				OSC3Count = OSC3CountOld - subtractValue; // Account for current TCNT != 0
+			}
+			unfinished &= B11111011;
+		}
+		if(OSC4Count <= tcnt2plus1) {
+			OSC4Phase = !OSC4Phase;
+			if(OSC4Phase) {
+				bitSet(PORTD, OSC4PIN);
+			} else {
+				bitClear(PORTD, OSC4PIN);
+			}
+			if(OSC4CountUpdated) {
+				OSC4Count = OSC4CountNext - subtractValue; // Account for current TCNT != 0
+			} else {
+				OSC4Count = OSC4CountOld - subtractValue; // Account for current TCNT != 0
+			}
+			unfinished &= B11110111;
+		}
+		if(unfinished == 0) return;
+		oldTCNT2 = tcnt2;
+		tcnt2 = TCNT2;
+	}
+}
+	/*
+	if(OSC1Count <= 255) {
+		OSC1Phase = !OSC1Phase;
+		if(OSC1Phase) {
+			bitSet(PORTD, OSC1PIN);
 		} else {
-			silence = true;
+			bitClear(PORTD, OSC1PIN);
+		}
+		if(OSC1CountUpdated) {
+			OSC1Count = OSC1CountNext; // Account for current TCNT != 0
+		} else {
+			OSC1Count = OSC1CountOld; // Account for current TCNT != 0
 		}
 	}
-	nextPulse = 0;
-}
-
-void timer2ISR()
-{
-	nextBeat = 1;
-}
+	if(OSC2Count <= 255) {
+		OSC2Phase = !OSC2Phase;
+		if(OSC2Phase) {
+			bitSet(PORTD, OSC2PIN);
+		} else {
+			bitClear(PORTD, OSC2PIN);
+		}
+		if(OSC2CountUpdated) {
+			OSC2Count = OSC2CountNext; // Account for current TCNT != 0
+		} else {
+			OSC2Count = OSC2CountOld; // Account for current TCNT != 0
+		}
+	}
+	if(OSC3Count <= 255) {
+		OSC3Phase = !OSC3Phase;
+		if(OSC3Phase) {
+			bitSet(PORTD, OSC3PIN);
+		} else {
+			bitClear(PORTD, OSC3PIN);
+		}
+		if(OSC3CountUpdated) {
+			OSC3Count = OSC3CountNext; // Account for current TCNT != 0
+		} else {
+			OSC3Count = OSC3CountOld; // Account for current TCNT != 0
+		}
+	}
+	if(OSC4Count <= 255) {
+		OSC4Phase = !OSC4Phase;
+		if(OSC4Phase) {
+			bitSet(PORTD, OSC4PIN);
+		} else {
+			bitClear(PORTD, OSC4PIN);
+		}
+		if(OSC3CountUpdated) {
+			OSC4Count = OSC4CountNext; // Account for current TCNT != 0
+		} else {
+			OSC4Count = OSC4CountOld; // Account for current TCNT != 0
+		}
+	}
+	*/
