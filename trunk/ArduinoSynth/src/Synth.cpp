2b0afa1ed9875ca43472e6f0d089736ae6453d05
glenn_thelen@yahoo.com <glenn_thelen@yahoo.com@9aec49c6-d00d-11de-9e88-ff79fe1e0c1e>
2014-11-30 23:23:43 +0000

    ArduinoSynth: 4 VFO oscillators using Timer2

--- trunk/ArduinoSynth/src/Synth.cpp
+++ trunk/ArduinoSynth/src/Synth.cpp
@@ -29,23 +29,25 @@ const float scale[] = {1.0, 17.0 / 16.0, 9.0 / 8.0, 6.0 / 5.0, 5.0 / 4.0, 4.0 / 
 int scaleIndex = 0;
 // Arrays not used for faster speed
 // Set to false when OSCxFullCount is being updated in case of interrupt
-volatile bool timer2Loop = false;
+volatile unsigned int timer2CountStart = 16;
+volatile unsigned int timer2Count = 0;
+volatile unsigned char numTimer2Overflows = 0;
 volatile bool OSC1CountUpdated = true;
 volatile bool OSC2CountUpdated = true;
 volatile bool OSC3CountUpdated = true;
 volatile bool OSC4CountUpdated = true;
-volatile int OSC1CountOld = 0;
-volatile int OSC2CountOld = 0;
-volatile int OSC3CountOld = 0;
-volatile int OSC4CountOld = 0;
-volatile int OSC1Count = 0;
-volatile int OSC2Count = 0;
-volatile int OSC3Count = 0;
-volatile int OSC4Count = 0;
-volatile int OSC1CountNext = 0;
-volatile int OSC2CountNext = 0;
-volatile int OSC3CountNext = 0;
-volatile int OSC4CountNext = 0;
+volatile unsigned int OSC1CountOld = 0;
+volatile unsigned int OSC2CountOld = 0;
+volatile unsigned int OSC3CountOld = 0;
+volatile unsigned int OSC4CountOld = 0;
+volatile unsigned int OSC1Count = 0;
+volatile unsigned int OSC2Count = 0;
+volatile unsigned int OSC3Count = 0;
+volatile unsigned int OSC4Count = 0;
+volatile unsigned int OSC1CountNext = 0;
+volatile unsigned int OSC2CountNext = 0;
+volatile unsigned int OSC3CountNext = 0;
+volatile unsigned int OSC4CountNext = 0;
 volatile bool OSC1Phase = false;
 volatile bool OSC2Phase = false;
 volatile bool OSC3Phase = false;
@@ -60,9 +62,8 @@ bool OSC4Active = false;
 #define OSC4PIN 5
 volatile bool nextBeat = false;
 volatile bool timer2InterruptInProgress = false;
-void handleTimer2Interrupt();
 void initTimer1(long microseconds);
-void initTimer2();
+void initTimer2(long cycles);
 void setFrequency1(float freq);
 void setFrequency2(float freq);
 void setFrequency3(float freq);
@@ -72,8 +73,13 @@ void setup()
 {
   pinMode(13, OUTPUT);
   DDRD = DDRD | B11111100;  // sets pins 2 to 7 as outputs
-  initTimer1(8000000);
-  initTimer2();
+  setFrequency1(octave[0] * scale[scaleIndex]);
+  setFrequency2(octave[1] * scale[scaleIndex]);
+  setFrequency3(octave[2] * scale[scaleIndex]);
+  setFrequency4(octave[3] * scale[scaleIndex]);
+  initTimer1(256);
+  initTimer2(256);
+  TIMSK0 &= ~_BV(TOIE0); // disable timer0 overflow interrupt
   scaleIndex = 0;
 }
 
@@ -85,35 +91,47 @@ void loop() {
 		setFrequency3(octave[2] * scale[scaleIndex]);
 		setFrequency4(octave[3] * scale[scaleIndex]);
 		scaleIndex = (scaleIndex + 1) % 12;
+		//delay(1000);
 	}
 }
 
 // Timer 1 Code:
 
-void initTimer1(long microseconds) {
+void initTimer1(long cycles) {
 	  TCCR1A = 0;                 // clear control register A
 	  TCCR1B = _BV(WGM13);        // set mode 8: phase and frequency correct pwm, stop the timer
-	  long cycles = 65535;                                // the counter runs backwards after TOP, interrupt is at BOTTOM so divide microseconds by 2
+	                          // the counter runs backwards after TOP, interrupt is at BOTTOM so divide microseconds by 2
 	  unsigned char oldSREG = SREG;
 	  cli();							// Disable interrupts for 16 bit register access
 	  ICR1 = cycles;                    // ICR1 is TOP in p & f correct pwm mode
 	  SREG = oldSREG;
 	  TIMSK1 = _BV(TOIE1);
 	  TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
-	  TCCR1B |= _BV(CS11) | _BV(CS10);  // prescale by /64
-	  sei();
+	  TCCR1B |= _BV(CS12);
 }
 
-ISR(TIMER1_OVF_vect)          // interrupt service routine that wraps a user defined function supplied by attachInterrupt
+
+void updateTimer1(long cycles) {
+	  unsigned char oldSREG = SREG;
+	  cli();							// Disable interrupts for 16 bit register access
+	  ICR1 = cycles;                    // ICR1 is TOP in p & f correct pwm mode
+	  SREG = oldSREG;
+}
+
+ISR(TIMER2_OVF_vect)          // interrupt service routine that wraps a user defined function supplied by attachInterrupt
 {
-	nextBeat = true;
+	timer2Count--;
+	if(timer2Count == 0) {
+		nextBeat = true;
+		timer2Count = 16;
+	}
 }
 
 // Timer 2 Code:
 
 void setFrequency1(float freqInHz) {
 	long cycles = round((1.0 / freqInHz) * 16000000.0);
-	cycles >>= 5;
+	cycles >>= 6;
 	OSC1CountUpdated = false;
 	OSC1CountNext = cycles;
 	OSC1CountUpdated = true;
@@ -123,7 +141,7 @@ void setFrequency1(float freqInHz) {
 
 void setFrequency2(float freqInHz) {
 	long cycles = round((1.0 / freqInHz) * 16000000.0);
-	cycles >>= 5;
+	cycles >>= 6;
 	OSC2CountUpdated = false;
 	OSC2CountNext = cycles;
 	OSC2CountUpdated = true;
@@ -133,7 +151,7 @@ void setFrequency2(float freqInHz) {
 
 void setFrequency3(float freqInHz) {
 	long cycles = round((1.0 / freqInHz) * 16000000.0);
-	cycles >>= 5;
+	cycles >>= 6;
 	OSC3CountUpdated = false;
 	OSC3CountNext = cycles;
 	OSC3CountUpdated = true;
@@ -143,7 +161,7 @@ void setFrequency3(float freqInHz) {
 
 void setFrequency4(float freqInHz) {
 	long cycles = round((1.0 / freqInHz) * 16000000.0);
-	cycles >>= 5;
+	cycles >>= 6;
 	OSC4CountUpdated = false;
 	OSC4CountNext = cycles;
 	OSC4CountUpdated = true;
@@ -151,7 +169,7 @@ void setFrequency4(float freqInHz) {
 	OSC4Active = true;
 }
 
-void initTimer2() {
+void initTimer2(long cycles) {
 
 	/* First disable the timer overflow interrupt while we're configuring */
 	TIMSK2 &= ~(1<<TOIE2);
@@ -163,31 +181,32 @@ void initTimer2() {
 	/* Disable Compare Match A interrupt enable (only want overflow) */
 	TIMSK2 &= ~(1<<OCIE2A);
 	/* Now configure the prescaler to CPU clock divided by 64 */
-	TCCR2B |= (1<<CS21) | (1<<CS20); // | (1<<CS20); // Set Bit
-	TCCR2B &= ~(1<<CS22); // Clear Bit
+	TCCR2B |= (1<<CS21) | (1<<CS20)| (1<<CS22); // | (1<<CS20); // Set Bit
+	//TCCR2B &= ~(1<<CS22); // Clear Bit
 	/* Finally load end enable the timer */
 	TCNT2 = 0;
 	TIMSK2 |= (1<<TOIE2);
+	timer2Count = 16;
 }
 
-// Called when timer 2 overflows
-ISR(TIMER2_OVF_vect) {
-	OSC1Count -= 256;
-	OSC2Count -= 256;
-	OSC3Count -= 256;
-	OSC4Count -= 256;
-	handleTimer2Interrupt();
-}
-
-void handleTimer2Interrupt() {
-	unsigned char tcnt2 = TCNT2;
-	unsigned char tcnt2plus1 = TCNT2;
-	unsigned char oldTCNT2 = tcnt2;
-	unsigned char unfinished = B00001111;
-	while(tcnt2 <= 253) {
-		unsigned char tcnt2plus1 = tcnt2 + 2;
-		signed int subtractValue = 256 - tcnt2;
-		if(OSC1Count <= tcnt2plus1) {
+// Called when timer 1 overflows
+ISR(TIMER1_OVF_vect) {
+	unsigned int minCycles = OSC1Count;
+	if(OSC2Count < minCycles) minCycles = OSC2Count;
+	if(OSC3Count < minCycles) minCycles = OSC3Count;
+	if(OSC4Count < minCycles) minCycles = OSC4Count;
+	if(minCycles > 0) {
+		OSC1Count -= minCycles;
+		OSC2Count -= minCycles;
+		OSC3Count -= minCycles;
+		OSC4Count -= minCycles;
+		//TIMSK2 &= ~(1<<TOIE2);
+		ICR1 = minCycles;
+		TCNT1 = minCycles;
+		//TIMSK2 &= (1<<TOIE2);
+		return;
+	} else {
+		if(OSC1Count == 0) {
 			OSC1Phase = !OSC1Phase;
 			if(OSC1Phase) {
 				bitSet(PORTD, OSC1PIN);
@@ -195,13 +214,12 @@ void handleTimer2Interrupt() {
 				bitClear(PORTD, OSC1PIN);
 			}
 			if(OSC1CountUpdated) {
-				OSC1Count = OSC1CountNext - subtractValue; // Account for current TCNT != 0
+				OSC1Count = OSC1CountNext; // Account for current TCNT != 0
 			} else {
-				OSC1Count = OSC1CountOld - subtractValue; // Account for current TCNT != 0
+				OSC1Count = OSC1CountOld; // Account for current TCNT != 0
 			}
-			unfinished &= B11111110;
 		}
-		if(OSC2Count <= tcnt2plus1) {
+		if(OSC2Count == 0) {
 			OSC2Phase = !OSC2Phase;
 			if(OSC2Phase) {
 				bitSet(PORTD, OSC2PIN);
@@ -209,13 +227,12 @@ void handleTimer2Interrupt() {
 				bitClear(PORTD, OSC2PIN);
 			}
 			if(OSC2CountUpdated) {
-				OSC2Count = OSC2CountNext - subtractValue; // Account for current TCNT != 0
+				OSC2Count = OSC2CountNext; // Account for current TCNT != 0
 			} else {
-				OSC2Count = OSC2CountOld - subtractValue; // Account for current TCNT != 0
+				OSC2Count = OSC2CountOld; // Account for current TCNT != 0
 			}
-			unfinished &= B11111101;
 		}
-		if(OSC3Count <= tcnt2plus1) {
+		if(OSC3Count == 0) {
 			OSC3Phase = !OSC3Phase;
 			if(OSC3Phase) {
 				bitSet(PORTD, OSC3PIN);
@@ -223,13 +240,12 @@ void handleTimer2Interrupt() {
 				bitClear(PORTD, OSC3PIN);
 			}
 			if(OSC3CountUpdated) {
-				OSC3Count = OSC3CountNext - subtractValue; // Account for current TCNT != 0
+				OSC3Count = OSC3CountNext; // Account for current TCNT != 0
 			} else {
-				OSC3Count = OSC3CountOld - subtractValue; // Account for current TCNT != 0
+				OSC3Count = OSC3CountOld; // Account for current TCNT != 0
 			}
-			unfinished &= B11111011;
 		}
-		if(OSC4Count <= tcnt2plus1) {
+		if(OSC4Count == 0) {
 			OSC4Phase = !OSC4Phase;
 			if(OSC4Phase) {
 				bitSet(PORTD, OSC4PIN);
@@ -237,68 +253,22 @@ void handleTimer2Interrupt() {
 				bitClear(PORTD, OSC4PIN);
 			}
 			if(OSC4CountUpdated) {
-				OSC4Count = OSC4CountNext - subtractValue; // Account for current TCNT != 0
+				OSC4Count = OSC4CountNext; // Account for current TCNT != 0
 			} else {
-				OSC4Count = OSC4CountOld - subtractValue; // Account for current TCNT != 0
+				OSC4Count = OSC4CountOld; // Account for current TCNT != 0
 			}
-			unfinished &= B11110111;
 		}
-		if(unfinished == 0) return;
-		oldTCNT2 = tcnt2;
-		tcnt2 = TCNT2;
+		minCycles = OSC1Count;
+		if(OSC2Count < minCycles) minCycles = OSC2Count;
+		if(OSC3Count < minCycles) minCycles = OSC3Count;
+		if(OSC4Count < minCycles) minCycles = OSC4Count;
+		OSC1Count -= minCycles;
+		OSC2Count -= minCycles;
+		OSC3Count -= minCycles;
+		OSC4Count -= minCycles;
+		//TIMSK2 &= ~(1<<TOIE2);
+		ICR1 = minCycles;
+		TCNT1 = minCycles;
+		//TIMSK2 &= (1<<TOIE2);
 	}
 }
-	/*
-	if(OSC1Count <= 255) {
-		OSC1Phase = !OSC1Phase;
-		if(OSC1Phase) {
-			bitSet(PORTD, OSC1PIN);
-		} else {
-			bitClear(PORTD, OSC1PIN);
-		}
-		if(OSC1CountUpdated) {
-			OSC1Count = OSC1CountNext; // Account for current TCNT != 0
-		} else {
-			OSC1Count = OSC1CountOld; // Account for current TCNT != 0
-		}
-	}
-	if(OSC2Count <= 255) {
-		OSC2Phase = !OSC2Phase;
-		if(OSC2Phase) {
-			bitSet(PORTD, OSC2PIN);
-		} else {
-			bitClear(PORTD, OSC2PIN);
-		}
-		if(OSC2CountUpdated) {
-			OSC2Count = OSC2CountNext; // Account for current TCNT != 0
-		} else {
-			OSC2Count = OSC2CountOld; // Account for current TCNT != 0
-		}
-	}
-	if(OSC3Count <= 255) {
-		OSC3Phase = !OSC3Phase;
-		if(OSC3Phase) {
-			bitSet(PORTD, OSC3PIN);
-		} else {
-			bitClear(PORTD, OSC3PIN);
-		}
-		if(OSC3CountUpdated) {
-			OSC3Count = OSC3CountNext; // Account for current TCNT != 0
-		} else {
-			OSC3Count = OSC3CountOld; // Account for current TCNT != 0
-		}
-	}
-	if(OSC4Count <= 255) {
-		OSC4Phase = !OSC4Phase;
-		if(OSC4Phase) {
-			bitSet(PORTD, OSC4PIN);
-		} else {
-			bitClear(PORTD, OSC4PIN);
-		}
-		if(OSC3CountUpdated) {
-			OSC4Count = OSC4CountNext; // Account for current TCNT != 0
-		} else {
-			OSC4Count = OSC4CountOld; // Account for current TCNT != 0
-		}
-	}
-	*/
